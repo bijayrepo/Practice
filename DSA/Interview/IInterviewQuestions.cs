981a13029354390cd65dc11d90f36108f8e639fc
BIJAY KUMAR MURMU <edubijaykumarmurmu@gmail.com>
2025-10-10 11:00:13 +0530

    Add StringPalindrome
    Signed-off-by: BIJAY KUMAR MURMU <edubijaykumarmurmu@gmail.com>

--- DSA/Interview/IInterviewQuestions.cs
+++ DSA/Interview/IInterviewQuestions.cs
@@ -16,11 +16,21 @@ namespace DSA.Interview
         {
             Console.WriteLine("Finding duplicate characters in the string: " + input);
         }
+        public virtual void LargestAndSmallestInArray(int[] numbers)
+        {
+            Console.WriteLine("Finding largest and smallest in the array: " + string.Join(", ", numbers));
+        }
+        public virtual void StringPalindrome(string input)
+        {
+            Console.WriteLine("Checking if the string is a palindrome: " + input);
+        }
     }
     internal interface IInterviewQuestions
     {
         public void ReverseString(string input);
         public void FindDuplicateCharacters(string input);
-        
+        public void LargestAndSmallestInArray(int[] numbers);
+        public void StringPalindrome(string input);
+
     }
 }
--- DSA/Interview/InterviewQuestions.cs
+++ DSA/Interview/InterviewQuestions.cs
@@ -11,10 +11,17 @@ namespace DSA.Interview
         public override void ReverseString(string input)
         {
             char[] charArray = input.ToCharArray();
-            Array.Reverse(charArray);
+            //Array.Reverse(charArray);
+            for (int i = 0, j = charArray.Length - 1; i < j; i++, j--)
+            {
+                char temp = charArray[i];
+                charArray[i] = charArray[j];
+                charArray[j] = temp;
+            }
             string reversedString = new string(charArray);
             Console.WriteLine("Reversed string: " + reversedString);
         }
+       
         public override void FindDuplicateCharacters(string input)
         {
             var characterCount = new Dictionary<char, int>();
@@ -29,14 +36,58 @@ namespace DSA.Interview
                     characterCount[c] = 1;
                 }
             }
+            foreach (char c in characterCount.Keys)
+            {
+                Console.WriteLine($"Character: {c}, Count: {characterCount[c]}");
+            }
             var duplicates = characterCount.Where(kvp => kvp.Value > 1).Select(kvp => kvp.Key);
             Console.WriteLine("Duplicate characters: " + string.Join(", ", duplicates));
+        }
+        public override void LargestAndSmallestInArray(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                Console.WriteLine("Array is empty.");
+                return;
+            }
+            int largest = numbers[0];
+            int smallest = numbers[0];
+            foreach (int num in numbers)
+            {
+                if (num > largest)
+                {
+                    largest = num;
+                }
+                if (num < smallest)
+                {
+                    smallest = num;
+                }
+            }
+            Console.WriteLine($"Largest number: {largest}, Smallest number: {smallest}");
+        }
+        public override void StringPalindrome(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            int left = 0,right=input.Length-1;
+            while(left<right)
+            {
+                if(charArray[left]!=charArray[right])
+                {
+                    Console.WriteLine($"The string '{input}' is not a palindrome.");
+                    return;
+                }
+                left++;
+                right--;
+            }
+            Console.WriteLine($"The string '{input}' is a palindrome.");
         }
         public static void Run()
         {
             InterviewQuestions iq = new InterviewQuestions();
-            iq.ReverseString("Hello World");
-            //iq.FindDuplicateCharacters("Programming");
+            iq.ReverseString("Bijay");
+            iq.FindDuplicateCharacters("Programming");
+            iq.LargestAndSmallestInArray(new int[]{ 34, -2, 45, 0, 11, -9, 78 });
+            iq.StringPalindrome("madam");
         }
     }
 }
