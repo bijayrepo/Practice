b8466491ec3c9e1f3b7b17c5b2cbb89d7b7de7da
Bijay kumar murmu <edubijaykumarmurmu@gmail.com>
2026-09-13 08:11:35 +0000

    Add MergeList.cs

---
+++ DSA/List/MergeList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.List
+{
+    internal class MergeList
+    {
+        public int[][] Merge(int[][] intervals)
+        {
+            if (intervals.Length == 0)
+                return new int[0][];
+
+
+            Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+            List<int[]> merged = new List<int[]>();
+            int[] currentInterval = intervals[0];
+            merged.Add(currentInterval);
+            foreach (var interval in intervals)
+            {
+                if (interval[0] <= currentInterval[1])
+                {
+                    currentInterval[1] = Math.Max(currentInterval[1], interval[1]);
+                }
+                else
+                {
+                    currentInterval = interval;
+                    merged.Add(currentInterval);
+                }
+            }
+            return merged.ToArray();
+        }
+    }
+}
